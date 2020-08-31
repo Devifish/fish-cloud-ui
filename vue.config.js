edfecf7097b648fe76b9ffc3d3527600c8279041
Devifish <devifish@outlook.com>
2020-08-31 18:21:49 +0800

    add: 添加依赖CDN

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,20 @@ const isProd = process.env.NODE_ENV == "production";
 const cdn = {
   enable: isProd,
   externals: {
+    vue: "Vue",
+    vuex: "Vuex",
+    "vue-router": "VueRouter",
+    "ant-design-vue": "antd",
     axios: "axios"
   },
   css: [],
-  js: ["https://cdn.jsdelivr.net/npm/axios@0.20.0/dist/axios.min.js"]
+  js: [
+    "https://cdn.jsdelivr.net/npm/vue@3.0.0-rc.9/dist/vue.global.min.js",
+    "https://cdn.jsdelivr.net/npm/vuex@4.0.0-beta.4/dist/vuex.global.min.js",
+    "https://cdn.jsdelivr.net/npm/vue-router@4.0.0-beta.7/dist/vue-router.global.min.js",
+    "https://cdn.jsdelivr.net/npm/ant-design-vue@2.0.0-beta.4/dist/antd.min.js",
+    "https://cdn.jsdelivr.net/npm/axios@0.20.0/dist/axios.min.js"
+  ]
 };
 
 module.exports = {
