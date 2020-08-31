86ebb2751fa4bbe97041d2ea37fd4b11ecd4ea7b
Devifish <devifish@outlook.com>
2020-08-31 16:52:23 +0800

    add: 添加对CDN的支持

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
+const isProd = process.env.NODE_ENV == "production";
+const cdn = {
+  enable: isProd,
+  externals: {
+    axios: "axios"
+  },
+  css: [],
+  js: ["https://cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js"]
+};
+
 module.exports = {
+  devServer: {
+    port: 8080
+  },
+  productionSourceMap: false,
   css: {
+    sourceMap: false,
     loaderOptions: {
       less: {
         lessOptions: {
@@ -7,5 +22,16 @@ module.exports = {
         }
       }
     }
+  },
+  configureWebpack: {
+    externals: cdn.enable ? cdn.externals : []
+  },
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      const options = args[0];
+      options.js = cdn.enable ? cdn.js : [];
+      options.css = cdn.enable ? cdn.css : [];
+      return args;
+    });
   }
 };
