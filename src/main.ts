5cfd3556fdabaab428e856bd967d7ca3090f0f2e
Devifish <devifish@outlook.com>
2020-09-02 18:01:37 +0800

    change: 三方依赖迁移到library

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import { createApp } from "vue";
 import Router from "./router";
 import Store from "./store";
 import Axios from "./library/axios";
-import Antd from "ant-design-vue";
+import Antd from "./library/ant-design";
 import App from "./App.vue";
-import "ant-design-vue/dist/antd.less";
 
 createApp(App)
   .use(Store)
