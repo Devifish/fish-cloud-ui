f89aea7d891af230f0a4f561f400d0409c8b128e
Devifish <devifish@outlook.com>
2020-09-03 17:50:11 +0800

    update: 优化代码

--- src/utils/path-matcher.ts
+++ src/utils/path-matcher.ts
@@ -1,8 +1,18 @@
+export interface PathMatcher {
+  /**
+   * 是否匹配传入的路径
+   * 存在返回 true
+   *
+   * @param path 路径
+   */
+  match(path: string): boolean;
+}
+
 /**
- * 路径匹配器
+ * Ant样式路径匹配器
  * 可用于匹配路径是否存在
  */
-class PathMatcher {
+class AntPathMatcher implements PathMatcher {
   private pathMatcherRegexp: Array<RegExp>;
 
   constructor(urls: Array<string>) {
@@ -16,23 +26,13 @@ class PathMatcher {
 
       return new RegExp(`^${item}$`);
     });
-
-    console.log(this.pathMatcherRegexp);
   }
 
-  /**
-   * 是否匹配传入的路径
-   * 存在返回 true
-   *
-   * @param path 路径
-   */
   match(path: string): boolean {
     return this.pathMatcherRegexp.some(regexp => regexp.test(path));
   }
 }
 
-export function createPathMatcher(urls: Array<string>) {
-  return new PathMatcher(urls);
+export function createPathMatcher(urls: Array<string>): PathMatcher {
+  return new AntPathMatcher(urls);
 }
-
-export default PathMatcher;
