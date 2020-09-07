8d3e1a56dce95fd99c805c67715dad20c47e1cc5
Devifish <devifish@outlook.com>
2020-09-07 16:39:45 +0800

    add: 添加对OAuth2的支持

--- src/library/axios.ts
+++ src/library/axios.ts
@@ -1,4 +1,4 @@
-import Axios, { AxiosRequestConfig } from "axios";
+import Axios, { AxiosRequestConfig, AxiosTransformer } from "axios";
 import { App } from "vue";
 import { message } from "ant-design-vue";
 
@@ -24,12 +24,49 @@ declare module "axios" {
   }
 }
 
+class OAuth2Token {
+  readonly TOKEN_HEADER = "Authorization";
+
+  accessToken: string;
+  tokenType: string;
+
+  constructor(accessToken: string, tokenType: string) {
+    this.accessToken = token.accessToken;
+    this.tokenType = token.tokenType;
+  }
+
+  tokenData() {
+    const tokenValue = `${this.tokenType} ${this.accessToken}`;
+    return {
+      header: this.TOKEN_HEADER,
+      value: tokenValue
+    };
+  }
+}
+
+// 公共Axios实例
 const axios = Axios.create({
   baseURL: "https://cloud.devifish.cn",
   timeout: 10000,
   withCredentials: true
 });
 
+// 用户Token
+var token: OAuth2Token;
+
+// Request 过滤器
+axios.interceptors.request.use(request => {
+  const { headers } = request;
+
+  // OAuth2用户Token注入
+  if (token) {
+    const { header, value } = token.tokenData();
+    headers[header] = value;
+  }
+  return request;
+});
+
+// Response 过滤器
 axios.interceptors.response.use(
   response => {
     const { data } = response;
@@ -74,6 +111,11 @@ axios.interceptors.response.use(
 export default <AxiosLibrary>{
   install(app: App) {
     app.config.globalProperties.$http = axios;
+  },
+  config: {
+    authorization(accessToken: string, tokenType: string) {
+      token = new OAuth2Token(accessToken, tokenType);
+    }
   },
 
   // 暴露 Axios 方法
