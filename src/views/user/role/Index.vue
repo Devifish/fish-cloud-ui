af7a287787b3b405709f938ccb96e00f38f68cd8
Devifish <devifish@outlook.com>
2020-09-19 11:38:06 +0800

    update: 完善用户角色列表

--- src/views/user/role/Index.vue
+++ src/views/user/role/Index.vue
@@ -1,28 +1,17 @@
 <template>
   <a-card title="内容筛选">
-    <a-form layout="inline" :model="params">
+    <a-form layout="inline" :model="params" @submit="search">
       <a-form-item label="角色名称">
-        <a-input v-model:value="params.name" placeholder="请输入角色名称">
-          <template v-slot:prefix><UserOutlined style="color:rgba(0,0,0,.25)"/></template>
-        </a-input>
+        <a-input v-model:value="params.name" placeholder="请输入角色名称" />
       </a-form-item>
       <a-form-item label="角色编码">
-        <a-input v-model:value="params.code" placeholder="请输入角色编码">
-          <template v-slot:prefix><UserOutlined style="color:rgba(0,0,0,.25)"/></template>
-        </a-input>
-      </a-form-item>
-      <a-form-item label="权限">
-        <a-input v-model:value="params.authority" placeholder="请输入权限">
-          <template v-slot:prefix><UserOutlined style="color:rgba(0,0,0,.25)"/></template>
-        </a-input>
+        <a-input v-model:value="params.code" placeholder="请输入角色编码" />
       </a-form-item>
       <a-form-item>
-        <a-button type="primary" html-type="submit" @click="loadData">
+        <a-button type="primary" html-type="submit">
           搜索
         </a-button>
-      </a-form-item>
-      <a-form-item>
-        <a-button type="primary" html-type="submit" @click="loadData">
+        <a-button style="margin-left: 10px">
           重置
         </a-button>
       </a-form-item>
@@ -30,21 +19,21 @@
   </a-card>
   <a-card title="用户列表">
     <a-table :data-source="list" :loading="state.loading" row-key="id" :bordered="true">
-      <a-table-column data-index="name" title="角色名称" align="center" width="15%" />
-      <a-table-column data-index="code" title="角色编码" align="center" width="10%" />
-      <a-table-column data-index="remark" title="描述" align="center" width="10%" />
-      <a-table-column title="权限" align="center" width="10%">
-        <template v-slot="{ record }">
-          <a-tag v-for="item of record.authorities" :key="item">
-            {{ item }}
-          </a-tag>
+      <a-table-column title="序号" align="center" width="5%">
+        <template v-slot="{ index }">
+          {{ index + 1 }}
         </template>
       </a-table-column>
+      <a-table-column data-index="name" title="角色名称" align="center" width="10%" />
+      <a-table-column data-index="code" title="角色编码" align="center" width="10%" />
+      <a-table-column data-index="remark" title="描述" align="center" width="20%" />
       <a-table-column data-index="createTime" title="创建时间" align="center" width="15%" />
       <a-table-column data-index="updateTime" title="更新时间" align="center" width="15%" />
       <a-table-column title="操作" width="10%">
         <template v-slot="{ record }">
           <span>
+            <a>编辑</a>
+            <a-divider type="vertical" />
             <a>删除</a>
           </span>
         </template>
@@ -67,6 +56,7 @@ export default defineComponent({
       page: PageData.init()
     });
 
+    // 请求参数
     const params = reactive({
       name: "",
       code: "",
@@ -88,12 +78,17 @@ export default defineComponent({
       state.page = PageData.of(data);
     }
 
+    function search() {
+      state.page = PageData.init();
+      loadData();
+    }
+
     onMounted(loadData);
     return {
       state,
       list,
       params,
-      loadData
+      search
     };
   }
 });
--- src/views/user/user/Index.vue
+++ src/views/user/user/Index.vue
@@ -1,13 +1,35 @@
 <template>
-  <a-card title="内容筛选"> </a-card>
+  <a-card title="内容筛选">
+    <a-form layout="inline" :model="params" @submit="search" ref="searchForm">
+      <a-form-item label="用户名">
+        <a-input v-model:value="params.username" placeholder="请输入用户名" />
+      </a-form-item>
+      <a-form-item label="手机号码">
+        <a-input v-model:value="params.telephone" placeholder="请输入手机号码" />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit">
+          搜索
+        </a-button>
+        <a-button style="margin-left: 10px" @click="$refs.searchForm.resetFields()">
+          重置
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </a-card>
   <a-card title="用户列表">
     <a-table
+      row-key="id"
       :data-source="list"
       :loading="state.loading"
-      row-key="id"
       :bordered="true"
+      :scroll="{ x: 1300 }"
     >
-    <a-table-column title="序号" align="center" width="5%"></a-table-column>
+      <a-table-column title="序号" align="center" width="5%">
+        <template v-slot="{ index }">
+          {{ index + 1 }}
+        </template>
+      </a-table-column>
       <a-table-column title="用户名" width="15%">
         <template v-slot="{ record }">
           <a-avatar :src="record.avatar" />
@@ -16,13 +38,21 @@
       </a-table-column>
       <a-table-column data-index="nickname" title="昵称" align="center" width="10%" />
       <a-table-column data-index="realname" title="真实姓名" align="center" width="10%" />
-      <a-table-column data-index="sex" title="性别" align="center" width="10%" />
+      <a-table-column data-index="sex" title="性别" align="center" width="5%">
+        <template v-slot="{ text }">
+          <a-tag v-if="text == 1" color="blue">男</a-tag>
+          <a-tag v-else-if="text == 2" color="red">女</a-tag>
+          <a-tag v-else>未设置</a-tag>
+        </template>
+      </a-table-column>
       <a-table-column data-index="telephone" title="手机号码" align="center" width="15%" />
       <a-table-column data-index="email" title="电子邮箱" align="center" width="15%" />
       <a-table-column data-index="birthday" title="生日" align="center" width="10%" />
-      <a-table-column title="操作" width="10%">
+      <a-table-column title="操作" width="200px" fixed="right">
         <template v-slot="{ record }">
           <span>
+            <a>编辑</a>
+            <a-divider type="vertical" />
             <a>删除</a>
           </span>
         </template>
@@ -45,6 +75,12 @@ export default defineComponent({
       page: PageData.init()
     });
 
+    // 请求参数
+    const params = reactive({
+      username: "",
+      telephone: ""
+    });
+
     // 列表数据
     const list = ref<Array<any>>();
 
@@ -52,7 +88,7 @@ export default defineComponent({
      * 加载页面数据
      */
     async function loadData() {
-      const { data } = await UserApi.selectPage(state.page, {});
+      const { data } = await UserApi.selectPage(state.page, params);
 
       // 获取数据成功
       state.loading = false;
@@ -60,10 +96,17 @@ export default defineComponent({
       state.page = PageData.of(data);
     }
 
+    function search() {
+      state.page = PageData.init();
+      loadData();
+    }
+
     onMounted(loadData);
     return {
       state,
-      list
+      params,
+      list,
+      search
     };
   }
 });
