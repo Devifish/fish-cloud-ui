0cf27de253cc780b0af67f964a4fbb29dc58be65
Devifish <devifish@outlook.com>
2020-09-19 18:22:15 +0800

    remove: 去除CDN加载

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,3 @@
-const isProd = process.env.NODE_ENV == "production";
-const cdn = {
-  enable: isProd,
-  externals: {
-    vue: "Vue",
-    vuex: "Vuex",
-    "vue-router": "VueRouter",
-    axios: "axios"
-  },
-  css: [],
-  js: [
-    "https://cdn.jsdelivr.net/npm/vue@3.0.0-rc.10/dist/vue.runtime.global.prod.js",
-    "https://cdn.jsdelivr.net/npm/vuex@4.0.0-beta.4/dist/vuex.global.prod.js",
-    "https://cdn.jsdelivr.net/npm/vue-router@4.0.0-beta.9/dist/vue-router.global.prod.js",
-    "https://cdn.jsdelivr.net/npm/axios@0.20.0/dist/axios.min.js"
-  ]
-};
-
 module.exports = {
   devServer: {
     port: 8080
@@ -31,16 +13,5 @@ module.exports = {
         }
       }
     }
-  },
-  configureWebpack: {
-    externals: cdn.enable ? cdn.externals : []
-  },
-  chainWebpack: config => {
-    config.plugin("html").tap(args => {
-      const options = args[0];
-      options.js = cdn.enable ? cdn.js : [];
-      options.css = cdn.enable ? cdn.css : [];
-      return args;
-    });
   }
 };
