1f30344e2bfb7d20066110b6562d649be0661c01
Devifish <devifish@outlook.com>
2020-09-22 11:37:41 +0800

    update: 防止弹出大量消息

--- src/library/ant-design.ts
+++ src/library/ant-design.ts
@@ -1,4 +1,10 @@
-import Antd from "ant-design-vue";
+import Antd, { message } from "ant-design-vue";
 import "ant-design-vue/dist/antd.less";
+
+// 防止弹出大量消息
+message.config({
+  duration: 2,
+  maxCount: 1
+});
 
 export default Antd;
