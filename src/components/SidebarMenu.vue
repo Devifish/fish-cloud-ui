266fa75c843ed03843b2ab6cd7ef5cc381b150aa
Devifish <devifish@outlook.com>
2020-10-15 15:54:04 +0800

    update: 优化svg图标实现

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -1,19 +1,19 @@
 <template>
   <a-sub-menu v-if="data.children?.length > 0" :key="data.id" v-bind="menuProps">
     <template v-slot:title>
-      <icon v-if="data.icon" :component="{ template: data.icon }" />
+      <component v-if="data.icon" :is="svg2icon(data.icon)" />
       <span>{{ data.name }}</span>
     </template>
     <sidebar-menu v-for="item of data.children" :data="item" :key="item.id" />
   </a-sub-menu>
   <a-menu-item v-else :key="data.id" v-bind="menuProps">
-    <icon v-if="data.icon" :component="{ template: data.icon }" />
+    <component v-if="data.icon" :is="svg2icon(data.icon)" />
     {{ data.name }}
   </a-menu-item>
 </template>
 
 <script lang="ts">
-import { defineComponent, createVNode, Component } from "vue";
+import { defineComponent, createVNode } from "vue";
 import { Menu } from "ant-design-vue";
 import Icon from "@ant-design/icons-vue";
 
@@ -23,9 +23,6 @@ const DEFAULT_PROPS = {
 
 export default defineComponent({
   name: "SidebarMenu",
-  components: {
-    Icon
-  },
   props: {
     ...(Menu.SubMenu as any).props,
     ...DEFAULT_PROPS
@@ -39,8 +36,14 @@ export default defineComponent({
       delete menuProps[key];
     }
 
+    function svg2icon(svg: string) {
+      const component = defineComponent({ template: svg });
+      return createVNode(Icon, { component });
+    }
+
     return {
-      menuProps
+      menuProps,
+      svg2icon
     };
   }
 });
